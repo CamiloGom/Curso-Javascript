50cf738a421db95c11fc5f53d720d816554270c8
Camilo Gómez <122405471+CamiloGom@users.noreply.github.com>
2023-01-24 19:38:32 -0300

    Update scripts.js

--- 1ra Pre-Entrega - Javascript - Camilo Gómez/js/scripts.js
+++ 1ra Pre-Entrega - Javascript - Camilo Gómez/js/scripts.js
@@ -1,59 +1,41 @@
 // Funcion calculadora de INTERES COMPUESTO
-function interesCompuesto (){
-    // funcion para calcular potencia (necesaria para la formula de INTERES COMPUESTO)
-    function factorPotencia (base, exponente) {
-        for(let i = 0; i < exponente; i++){
-            resultadoPotencia = resultadoPotencia * base
-        }
-        return resultadoPotencia
-    }
-    factorPotencia(FACTOR, tiempoMeses)
-    // Calculo de Capital final e interes acumulado
-    capitalFinal= (montoInvertir * resultadoPotencia).toFixed(2)
-    interesAcumulado = (capitalFinal - montoInvertir).toFixed(2)
-    console.log(`El INTERES COMPUESTO acumulado será de $${interesAcumulado}`)
-    console.log(`Al finalizar los ${tiempoMeses} meses usted tendría $${capitalFinal} disponible`)
+function interesCompuesto() {
+    let capitalFinal = montoInvertir * (1 + interesMensual)**tiempoMeses
+    let interesAcumulado = capitalFinal - montoInvertir
+    console.log(`El INTERES COMPUESTO acumulado será de $${interesAcumulado.toFixed(2)}`)
+    console.log(`Al finalizar los ${tiempoMeses} meses usted tendría $${capitalFinal.toFixed(2)} disponible`)
 }
 //Funcion calculadora de INTERES SIMPLE
-function interesSimple (){
-    interesAcumulado = montoInvertir * interesMensual * tiempoMeses
-    capitalFinal = interesAcumulado + montoInvertir
-    console.log(`El INTERES SIMPLE acumulado será de $${interesAcumulado}`)
-    console.log(`Al finalizar los ${tiempoMeses} meses usted tendría $${capitalFinal} disponible`)
+function interesSimple() {
+    let interesAcumulado = montoInvertir * interesMensual * tiempoMeses
+    let capitalFinal = interesAcumulado + montoInvertir
+    console.log(`El INTERES SIMPLE acumulado será de $${interesAcumulado.toFixed(2)}`)
+    console.log(`Al finalizar los ${tiempoMeses} meses usted tendría $${capitalFinal.toFixed(2)} disponible`)
 }
-// ENTRADA A LA APLICACION y seleccion de tipo de calculo
-let opcion = ""
+// ENTRADA A LA APLICACION y seleccion de tipo de cálculo
 alert("BIENVENIDO A LA CALCULADORA DE INTERESES\nHaga CLICK en aceptar para continuar")
-while (opcion != "SIMP" || opcion!= "COMP" || opcion!= "AMBOS") {
-    opcion = prompt("Ingrese SIMP para calcular INTERES SIMPLE \nIngrese COMP para calcular INTERES COMPUESTO\nIngrese AMBOS para calcular los dos tipos de intereses")
-    if(opcion=="SIMP" || opcion=="COMP" || opcion =="AMBOS" ){
-        break
-    }else {
-        alert("POR FAVOR INGRESE UN DATO VALIDO")
-    }
-}
 //Ingreso de datos
 let montoInvertir = parseFloat(prompt("Ingrese MONTO a invertir en pesos"))
 let interesAnual = parseFloat(prompt("Ingrese TASA de INTERES ANUAL"))
 let tiempoMeses = parseInt(prompt("Ingrese cuantos MESES mantendrá su inversión"))
-let interesMensual = (interesAnual/12) /100
-let porcentajePesos = montoInvertir * interesMensual /100
-let resultadoPotencia = 1
-let capitalFinal = 0
-let interesAcumulado = 0
-const FACTOR = (1+interesMensual)
-console.log (`Su interes mensual será ${interesMensual*100}%, lo que equivale a $${porcentajePesos*100} `)
-// Llamada a las funciones segun tipo de calculo elegido
+let interesMensual = (interesAnual / 12) / 100
+let porcentajePesos = montoInvertir * interesMensual / 100
+let opcion = prompt("Ingrese SIMP para calcular INTERES SIMPLE \nIngrese COMP para calcular INTERES COMPUESTO\nIngrese AMBOS para calcular los dos tipos de intereses")
+while (opcion != "SIMP" && opcion != "COMP" && opcion != "AMBOS") {
+    alert("POR FAVOR INGRESE UN DATO VALIDO")
+    opcion = prompt("Ingrese SIMP para calcular INTERES SIMPLE \nIngrese COMP para calcular INTERES COMPUESTO\nIngrese AMBOS para calcular los dos tipos de intereses")
+}
+console.log(`Su interes mensual será ${(interesMensual * 100).toFixed(4)}%, lo que equivale a $${(porcentajePesos*100).toFixed(2)} `)
+// Llamada a las funciones según tipo de cálculo elegido
 switch (opcion) {
     case "SIMP":
         interesSimple()
-    break;
+        break;
     case "COMP":
         interesCompuesto()
-    break;
+        break;
     case "AMBOS":
         interesSimple()
         interesCompuesto()
-    break;
- }
-    
+        break;
+}
